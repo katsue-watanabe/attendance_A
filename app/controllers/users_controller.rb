465cc57dac4cc5fa1c3218b0c2bcd3d42ebe0482
katsue-watanabe <k-losmama@docomo.ne.jp>
2022-01-29 18:24:20 +0900

    日本語化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
     end
 
     def correct_user
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def admin_user
