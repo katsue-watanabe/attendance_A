cf01ec0783fe22c8643367772087057744c6293a
katsue-watanabe <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>
2022-07-24 02:08:57 +0000

    fix-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220712055430) do
+ActiveRecord::Schema.define(version: 20220722205437) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20220712055430) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_started"
+    t.datetime "last_finished"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
@@ -62,8 +64,8 @@ ActiveRecord::Schema.define(version: 20220712055430) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.datetime "basic_work_time", default: "2022-07-18 23:00:00"
-    t.datetime "work_time", default: "2022-07-18 22:30:00"
+    t.datetime "basic_work_time", default: "2022-07-23 23:00:00"
+    t.datetime "work_time", default: "2022-07-23 22:30:00"
     t.time "designated_work_start_time", default: "2000-01-01 23:00:00"
     t.time "designated_work_end_time", default: "2000-01-01 08:00:00"
     t.boolean "superior", default: false
